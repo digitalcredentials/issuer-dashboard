576090a5732b8b0aa649ab021fa90968e36c443c
James Chartrand <jc.chartrand@gmail.com>
2026-02-11 08:45:44 -0500

    remove console output

--- app/lib/store.ts
+++ app/lib/store.ts
@@ -6,8 +6,6 @@ type holderBody = {name:string, email:string, did: string, org_id: string}
 type storeBody = queryBody | credBody | holderBody
 
 export const callStore = async (path : string, method: string, body?: storeBody) => {
-    console.log("the url:")
-    console.log
   try {
     const response = (method === 'GET') ?
         await fetch(`http://localhost:3000/${path}`)
@@ -24,7 +22,6 @@ export const callStore = async (path : string, method: string, body?: storeBody)
       throw new Error(`HTTP error! status: ${response.status}`);
     }
     const data = await response.json(); 
-    console.log(data);
     return data;
   } catch (error) {
     console.error('Fetch error:', error);
