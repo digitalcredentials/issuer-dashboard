10acd3750103454c192c4c4e45cd4b0c844e6029
James Chartrand <jc.chartrand@gmail.com>
2026-02-11 17:01:18 -0500

    minor formatting

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -1,5 +1,5 @@
 'use server';
- 
+
 import { signIn, auth } from '@/auth';
 
 import { AuthError } from 'next-auth';
@@ -21,7 +21,7 @@ const FormSchema = z.object({
   holder: z.string(),
   email: z.string(),
 });
- 
+
 const CreateCredential = FormSchema.omit({ id: true, status: true });
 const UpdateCredential = FormSchema.omit({ id: true, status: true });
 
@@ -37,22 +37,22 @@ export type State = {
 
 export async function createCredential(prevState: State, formData: FormData) {
 
-    const session = await auth(); // Get the current session
-    let userName;
-    if (!session?.user) {
-        throw new Error('You must be signed in to perform this action');
-    } else {
-        userName = session.user.email as string
-    }
+  const session = await auth(); // Get the current session
+  let userName;
+  if (!session?.user) {
+    throw new Error('You must be signed in to perform this action');
+  } else {
+    userName = session.user.email as string
+  }
 
-   const validatedFields = CreateCredential.safeParse({
+  const validatedFields = CreateCredential.safeParse({
     credTypeId: formData.get('credTypeId'),
     credName: formData.get('credName'),
     holder: formData.get('holder'),
     email: formData.get('email'),
   });
- 
-   // If form validation fails, return errors early. Otherwise, continue.
+
+  // If form validation fails, return errors early. Otherwise, continue.
   if (!validatedFields.success) {
     return {
       errors: validatedFields.error.flatten().fieldErrors,
@@ -60,12 +60,12 @@ export async function createCredential(prevState: State, formData: FormData) {
     };
   }
 
-   // Prepare data for insertion into the database
+  // Prepare data for insertion into the database
   const { credTypeId, email, credName, holder } = validatedFields.data;
- // TODO: want to directly deal with 404's using notFound()
+  // TODO: want to directly deal with 404's using notFound()
   try {
     // TODO the credType will be used to pick a vc template, populate it, and that populated template will be saved here to the store.
-    const result = await callStore('credential', 'POST', {holder_email: email, holder_name: holder, cred_name: credName, added_by: userName})
+    const result = await callStore('credential', 'POST', { holder_email: email, holder_name: holder, cred_name: credName, added_by: userName })
 
   } catch (error) {
     // We'll also log the error to the console for now
@@ -74,7 +74,7 @@ export async function createCredential(prevState: State, formData: FormData) {
       message: 'Database Error: Failed to Create Credential.',
     };
   }
- 
+
   revalidatePath('/dashboard/credentials');
   redirect('/dashboard/credentials');
 }
@@ -91,22 +91,22 @@ export async function updateCredential(
     holder: formData.get('holder'),
     email: formData.get('email'),
   });
- 
+
   if (!validatedFields.success) {
     return {
       errors: validatedFields.error.flatten().fieldErrors,
       message: 'Missing Fields. Failed to Update Credential.',
     };
   }
- 
+
   const { credTypeId, email, credName, holder } = validatedFields.data;
- 
+
   try {
-     const result = await callStore(`credential/${id}`, 'PUT', {holder_email: email, holder_name: holder, cred_name: credName})
+    const result = await callStore(`credential/${id}`, 'PUT', { holder_email: email, holder_name: holder, cred_name: credName })
   } catch (error) {
     return { message: 'Database Error: Failed to pdate credential.' };
   }
- 
+
   revalidatePath('/dashboard/credentials');
   redirect('/dashboard/credentials');
 }
@@ -114,10 +114,10 @@ export async function updateCredential(
 
 
 export async function deleteCredential(id: string) {
-    // TODO: not sure if we even want to allow deletions
-     throw new Error('Deletion not allowed');
+  // TODO: not sure if we even want to allow deletions
+  throw new Error('Deletion not allowed');
 
- // revalidatePath('/dashboard/credentials');
+  // revalidatePath('/dashboard/credentials');
 }
 
 export async function authenticate(
