cb09957bded6e1f6ed9129c80b97efdb3b760efc
James Chartrand <jc.chartrand@gmail.com>
2026-02-17 15:43:28 -0500

    use holder object and id for population and store calls

--- app/ui/credentials/create-form.tsx
+++ app/ui/credentials/create-form.tsx
@@ -18,7 +18,7 @@ export default function Form({ templates }: { templates: TemplateField[] }) {
   const initialState: State = { message: null, errors: {} };
   const [state, formAction] = useActionState(createCredential, initialState);
   const [isModalOpen, setIsModalOpen] = useState(false)
-  const [holder, setHolder] = useState({name:'', did: '', org_id: '', email: ''})
+  const [holder, setHolder] = useState({name:'', did: '', org_id: '', email: '', id: undefined})
   const findHolder = () => {
     setIsModalOpen(true);
   }
@@ -39,6 +39,12 @@ export default function Form({ templates }: { templates: TemplateField[] }) {
           </div>
         </ClientPortal>
       )}
+              <input
+                id="holderId"
+                name="holderId"
+                type="hidden"
+                defaultValue={holder.id}
+              />
       <div className="rounded-md bg-gray-50 p-4 md:p-6">
         {/* Credential Type */}
         <div className="mb-4">
@@ -106,7 +112,7 @@ export default function Form({ templates }: { templates: TemplateField[] }) {
         {/* Holder Name */}
         <div className="mb-4">
           <label htmlFor="holder" className="mb-2 block text-sm font-medium">
-            Choose a holder
+            Choose a holder (click to search)
           </label>
           <div className="relative mt-2 rounded-md">
             <div className="relative">
@@ -124,8 +130,8 @@ export default function Form({ templates }: { templates: TemplateField[] }) {
               <UserCircleIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
             </div>
                <div id="holder-error" aria-live="polite" aria-atomic="true">
-                {state.errors?.holder &&
-                  state.errors.holder.map((error: string) => (
+                {state.errors?.holderId &&
+                  state.errors.holderId.map((error: string) => (
                     <p className="mt-2 text-sm text-red-500" key={error}>
                       {error}
                     </p>
@@ -153,17 +159,33 @@ export default function Form({ templates }: { templates: TemplateField[] }) {
               />
               <AtSymbolIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
             </div>
-               <div id="email-error" aria-live="polite" aria-atomic="true">
-                {state.errors?.email &&
-                  state.errors.email.map((error: string) => (
-                    <p className="mt-2 text-sm text-red-500" key={error}>
-                      {error}
-                    </p>
-                  ))}
-                </div>
           </div>
         </div>
         
+
+                {/* Holder org id */}
+        <div className="mb-4">
+          <label htmlFor="org_id" className="mb-2 block text-sm font-medium">
+            Holder&apos;s internal organizational id
+          </label>
+          <div className="relative mt-2 rounded-md">
+            <div className="relative">
+              <input
+                readOnly
+                id="org_id"
+                name="org_id"
+                type="org_id"
+                defaultValue={holder.org_id}
+                placeholder="Automatically set from holder search"
+                className="peer block w-full rounded-md border border-gray-200 py-2 pl-10 text-sm outline-2 placeholder:text-gray-500"
+                aria-describedby="email-error"
+              />
+              <AtSymbolIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
+            </div>
+          </div>
+        </div>
+
+
           <div id="general-error" aria-live="polite" aria-atomic="true">
             {state.errors &&
                 <p className="mt-2 text-sm text-red-500">
