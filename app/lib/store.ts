325f14b3058f4c5bc7f070d462bb4d583a2813ff
James Chartrand <jc.chartrand@gmail.com>
2026-02-19 15:05:15 -0500

    add notification to store body typing

--- app/lib/store.ts
+++ app/lib/store.ts
@@ -3,7 +3,8 @@
 type queryBody = {queryTerm:string,currentPage?:number};
 type credBody = {holder_id:string,cred_name: string, added_by?: string}
 type holderBody = {name:string, email:string, did: string, org_id: string}
-type storeBody = queryBody | credBody | holderBody
+type notificationBody = {credential_id:string, email:string}
+type storeBody = queryBody | credBody | holderBody | notificationBody
 
 export const callStore = async (path : string, method: string, body?: storeBody) => {
   try {
