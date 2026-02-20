6318516472b9de35368a57c03aaaf227b73666fa
James Chartrand <jc.chartrand@gmail.com>
2026-02-20 15:46:37 -0500

    cleanup main collection page

--- app/collect/page.tsx
+++ app/collect/page.tsx
@@ -1,7 +1,8 @@
 'use server';
 import Form from '@/app/ui/collect/form';
 import { lusitana } from '@/app/ui/fonts';
-import { fetchHolderCredsByPickupToken, fetchHolderCredsById } from "@/app/lib/data";
+import { fetchHolderCredsByPickupToken } from "@/app/lib/data";
+import { notFound } from 'next/navigation';
 
 export default async function Page(props: {
   searchParams?: Promise<{
@@ -10,18 +11,15 @@ export default async function Page(props: {
 }) {
   const searchParams = await props.searchParams;
   const pickupToken = searchParams?.pickup_token || null;
-  const holderId = "4";
+
   let credentials;
   if (pickupToken) {
     credentials = await fetchHolderCredsByPickupToken(pickupToken)
-    console.log("the test result", credentials)
-  } 
-   // const credentials = await fetchHolderCredsById(holderId)
-  
-
+  } else {
+    notFound()
+  }
   return (
     <main className ="p-4">
-      <h1 className={`${lusitana.className} text-2xl p-4`}>You have the following credentials. Select one to download.</h1>
       <Form credentials={credentials}/>
     </main>
   );
