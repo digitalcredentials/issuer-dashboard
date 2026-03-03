ae56f1b1b917f678e4cd3b7a5076112ab575e582
James Chartrand <jc.chartrand@gmail.com>
2026-03-03 15:06:25 -0500

    add upload form

---
+++ app/ui/credentials/upload-form.tsx
@@ -0,0 +1,168 @@
+'use client';
+
+import { Template, Tenant } from '@/app/lib/definitions';
+import Link from 'next/link';
+import {
+  UserCircleIcon,
+  AtSymbolIcon,
+  TagIcon,
+  MagnifyingGlassCircleIcon
+} from '@heroicons/react/24/outline';
+import { Button } from '@/app/ui/button';
+import { createCredential, State } from '@/app/lib/actions';
+import { useActionState, useState } from 'react';
+
+export default function Form({ templates, tenants }: { templates: Template[], tenants: Tenant[] }) {
+  const initialState: State = { message: null, errors: {}, formData: {credName: undefined, tenantId: undefined, templateId: undefined}   };
+  const [state, formAction] = useActionState(createCredential, initialState);
+  return (
+     <form action={formAction}> 
+
+      <div className="rounded-md bg-gray-50 p-4 md:p-6">
+
+        {/* Credential template */}
+        <div className="mb-4">
+          <label htmlFor="template-id" className="mb-2 block text-sm font-medium">
+            Choose a template
+          </label>
+          <div className="relative">
+            <select
+              id="template-id"
+              name="templateId"
+              className="peer block w-full cursor-pointer rounded-md border border-gray-200 py-2 pl-10 text-sm outline-2 placeholder:text-gray-500"
+              defaultValue={state.formData?.templateId}
+              aria-describedby="template-id-error"
+            >
+              <option value="" disabled>
+                Select a template
+              </option>
+              {templates.map((template) => (
+                <option key={template.id} value={template.id}>
+                  {template.name}
+                </option>
+              ))}
+            </select>
+            <TagIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500" />
+          </div>
+          <div id="template-id-error" aria-live="polite" aria-atomic="true">
+            {state.errors?.templateId &&
+              state.errors.templateId.map((error: string) => (
+                <p className="mt-2 text-sm text-red-500" key={error}>
+                  {error}
+                </p>
+              ))}
+          </div>
+        </div>
+
+        {/* Tenant */}
+        <div className="mb-4">
+          <label htmlFor="tenant-id" className="mb-2 block text-sm font-medium">
+            Choose an issuer (who signs this credential)
+          </label>
+          <div className="relative">
+            <select
+              id="tenant-id"
+              name="tenantId"
+              className="peer block w-full cursor-pointer rounded-md border border-gray-200 py-2 pl-10 text-sm outline-2 placeholder:text-gray-500"
+              defaultValue={state.formData?.tenantId}
+              aria-describedby="tenant-id-error"
+            >
+              <option value="" disabled>
+                Select an issuer
+              </option>
+              {tenants.map((tenant) => (
+                <option key={tenant.id} value={tenant.id}>
+                  {tenant.name}
+                </option>
+              ))}
+            </select>
+            <TagIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500" />
+          </div>
+          <div id="tenant-id-error" aria-live="polite" aria-atomic="true">
+            {state.errors?.tenantId &&
+              state.errors.tenantId.map((error: string) => (
+                <p className="mt-2 text-sm text-red-500" key={error}>
+                  {error}
+                </p>
+              ))}
+          </div>
+        </div>
+
+ {/* Batch Name */}
+        <div className="mb-4">
+          <label htmlFor="credName" className="mb-2 block text-sm font-medium">
+            Specify a name for this batch (to later find it)
+          </label>
+          <div className="relative mt-2 rounded-md">
+            <div className="relative">
+              
+              <input
+                id="credName"
+                name="credName"
+                type="string"
+                defaultValue={state.formData?.credName}
+                placeholder="Enter a name for the batch"
+                className="peer block w-full rounded-md border border-gray-200 py-2 pl-10 text-sm outline-2 placeholder:text-gray-500"
+                aria-describedby="credName-error"
+              />
+              <MagnifyingGlassCircleIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
+            </div>
+               <div id="credName-error" aria-live="polite" aria-atomic="true">
+                {state.errors?.credName &&
+                  state.errors.credName.map((error: string) => (
+                    <p className="mt-2 text-sm text-red-500" key={error}>
+                      {error}
+                    </p>
+                  ))}
+                </div>
+          </div>
+        </div>
+
+
+        {/* Upload */}
+        <div className="mb-4">
+          <label htmlFor="holder" className="mb-2 block text-sm font-medium">
+            Upload your CSV file
+          </label>
+          <div className="relative mt-2 rounded-md">
+            <div className="relative">
+              <input
+                id="csv"
+                name="csv"
+                type="file"
+                className="peer block w-full rounded-md border border-gray-200 py-2 pl-10 text-sm outline-2 placeholder:text-gray-500"
+                aria-describedby="csv-error"
+              />
+              <UserCircleIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
+            </div>
+               <div id="csv-error" aria-live="polite" aria-atomic="true">
+                {state.errors?.csv &&
+                  state.errors.csv.map((error: string) => (
+                    <p className="mt-2 text-sm text-red-500" key={error}>
+                      {error}
+                    </p>
+                  ))}
+                </div>
+          </div>
+        </div>
+
+          <div id="general-error" aria-live="polite" aria-atomic="true">
+            {state.errors &&
+                <p className="mt-2 text-sm text-red-500">
+                  {state.message}
+                </p>
+              }
+          </div>
+      </div>
+      <div className="mt-6 flex justify-end gap-4">
+        <Link
+          href="/dashboard/credentials"
+          className="flex h-10 items-center rounded-lg bg-gray-100 px-4 text-sm font-medium text-gray-600 transition-colors hover:bg-gray-200"
+        >
+          Cancel
+        </Link>
+        <Button type="submit">Submit Batch</Button>
+      </div>
+    </form>
+  );
+}
