9436680f9cb448c0ab4fbde7500e15d2b5cb60c1
James Chartrand <jc.chartrand@gmail.com>
2026-03-06 12:08:33 -0500

    add holder batch upload button

--- app/ui/holders/buttons.tsx
+++ app/ui/holders/buttons.tsx
@@ -2,13 +2,25 @@ import { PencilIcon, PlusIcon, TrashIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
 import { deleteHolder } from '@/app/lib/holderActions';
 
-export function CreateHolder() {
+export function AddHolder() {
   return (
     <Link
       href="/dashboard/holders/create"
       className="flex h-10 items-center rounded-lg bg-blue-600 px-4 text-sm font-medium text-white transition-colors hover:bg-blue-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600"
     >
       <span className="hidden md:block">Add Holder</span>{' '}
+      <PlusIcon className="h-5 md:ml-4" />
+    </Link>
+  );
+}
+
+export function UploadHolders() {
+  return (
+    <Link
+      href="/dashboard/holders/upload"
+      className="flex h-10 items-center rounded-lg bg-blue-600 px-4 text-sm font-medium text-white transition-colors hover:bg-blue-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600"
+    >
+      <span className="hidden md:block">Upload Holders</span>{' '}
       <PlusIcon className="h-5 md:ml-4" />
     </Link>
   );
