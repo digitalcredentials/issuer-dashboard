04f9786c6c221b5dee2ab395ce0c338366c586d6
James Chartrand <jc.chartrand@gmail.com>
2026-03-06 12:08:57 -0500

    add credentials batch upload button

--- app/ui/credentials/buttons.tsx
+++ app/ui/credentials/buttons.tsx
@@ -2,19 +2,29 @@ import { InboxIcon, PencilIcon, PlusIcon, TrashIcon } from '@heroicons/react/24/
 import Link from 'next/link';
 import { deleteCredential, notifyHolder } from '@/app/lib/actions';
 
-export function CreateCredential() {
+export function AddCredential() {
   return (
     <Link
-      href="/dashboard/credentials/create"
+      href="/dashboard/credentials/add"
       className="flex h-10 items-center rounded-lg bg-blue-600 px-4 text-sm font-medium text-white transition-colors hover:bg-blue-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600"
     >
-      <span className="hidden md:block">Create Credential</span>{' '}
+      <span className="hidden md:block">Add Credential</span>{' '}
       <PlusIcon className="h-5 md:ml-4" />
     </Link>
   );
 }
 
-
+export function UploadCredentials() {
+  return (
+    <Link
+      href="/dashboard/credentials/upload"
+      className="flex h-10 items-center rounded-lg bg-blue-600 px-4 text-sm font-medium text-white transition-colors hover:bg-blue-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600"
+    >
+      <span className="hidden md:block">Upload Batch</span>{' '}
+      <PlusIcon className="h-5 md:ml-4" />
+    </Link>
+  );
+}
 
 export function UpdateCredential({ id }: { id: string }) {
   return (
