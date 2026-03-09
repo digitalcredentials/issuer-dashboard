b7b855bb953d257520a4e3c270e24b57e9056241
James Chartrand <jc.chartrand@gmail.com>
2026-03-09 16:15:25 -0400

    add zod row validation, email duplicate checks in csv and db, error handling

--- app/lib/holders/uploadAction.ts
+++ app/lib/holders/uploadAction.ts
@@ -3,15 +3,17 @@
 import { auth } from '@/auth';
 
 import { callStore } from '../store';
-import * as fastcsv from '@fast-csv/parse';
+import { parse } from '@fast-csv/parse';
 import { Readable } from 'node:stream';
 import { z } from 'zod';
 import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
+import { Holder } from '../definitions';
 
 const MAX_UPLOAD_SIZE = 1024 * 1024 * 3;
 const ACCEPTED_MIME_TYPES = ["text/csv", "text/comma-separated-values", "text/x-csv", "application/csv", "application/vnd.ms-excel", "text/plain"];
 
+// zod schema object for the form upload
 const FormSchema = z.object({
   csv: z
     .instanceof(File, { message: "Expected a file" })
@@ -28,20 +30,46 @@ const FormSchema = z.object({
     )
 });
 
+// zod schema object for each csv row
+const CsvSchema = z.object({
+  name: z.string().trim().min(1, { message: "A holder name is required" }),
+  email: z.string().email().trim().min(1, { message: "A valid email is required" }),
+  did: z.string({invalid_type_error: 'The holderDID must be a string.'}),
+  org_id: z.string({invalid_type_error: 'The holderOrgId must be a string.'})
+});
+
+// the state typing for this action
 export type State = {
   errors?: {
-    templateId?: string[];
-    tenantId?: string[];
-    csv?: string[];
-    batchName?: string[];
-
+    errorType?: 'existing-holders' | 'duplicate-emails' | 'csv-row' | 'network' | 'form' | 'csv-parsing' ;
+    formErrors?: {csv?: []};
+    message?: string | null;
+    existingList: any;
+    duplicateList: any;
+    fieldErrors: any;
   };
-  message?: string | null;
   formData: {
     csv: File | undefined;
   }
-
 };
+
+    /*  This upload:
+    1. Checks the incoming from values against ZOD schema. If errors, return.
+    
+    2. Processes the CSV and use Zod to check for missing names or missing/bad 
+        emails as we process the incoming rows.
+  
+    After all rows are processed:
+  
+    3. If there were ZOD errors then returns them.
+
+    4  If there are duplicate emails then returns them:
+	
+    5.  Calls the store to check for existing emails in the db.
+      If duplicates, returns them.
+    
+    6. sends the holders to the store
+ */
 
 export async function uploadHolders(prevState: State, formData: FormData) : Promise<any> {
 
@@ -54,58 +82,116 @@ export async function uploadHolders(prevState: State, formData: FormData) : Prom
   }
 
   const incomingFormValues = {
-    templateId: formData.get('templateId'),
-    tenantId: formData.get('tenantId'),
-    batchName: formData.get('batchName'),
     csv: formData.get('csv')
   }
-  const validatedFields = FormSchema.safeParse(incomingFormValues);
+  const formValidationResult = FormSchema.safeParse(incomingFormValues);
 
-  // If form validation fails, return errors early. Otherwise, continue.
-  if (!validatedFields.success) {
+  // If form validation fails, return errors. Otherwise, continue.
+  if (!formValidationResult.success) {
     return {
-      errors: validatedFields.error.flatten().fieldErrors,
-      message: 'Missing Fields. Failed to Create Credential.',
+      errors: {
+        errorType: 'form',
+        formErrors: formValidationResult.error.flatten().fieldErrors,
+        message: 'Missing Fields. Failed to process your CSV file.'
+      },
       formData: incomingFormValues
     };
   }
 
-  // Prepare data for insertion into the database
-  const { csv } = validatedFields.data;
-  const batch:[] = [];
-
-  const processRow = (row:any) => {
-  /*  1. check the row's data to make sure required fields are there, emails are emails, etc.
-    2. add each row to an array, also have to add the templateId and 
-    hmmmmmmmmm, will also have to do some kind of lookup of the holder portion to make sure it isn't in the db already??????? like by email?
-    ORRRRRRR, maybe do the holder upload separately.
-    hmmmmmm, problem I think is if a new holder entry comes in that overwrites an existing entry. do we just overwrite??? or ask to overwrite???
-    3. when done, post the array.
-    4. add a new endpoint that takes the array, sets up a transaction, inserts the rows with:
-    INSERT INTO table_name (column1, column2, column3)
-VALUES
-(value1_row1, value2_row1, value3_row1),
-(value1_row2, value2_row2, value3_row2),
-(value1_row3, value2_row3, value3_row3);. */
-   }
-
-  Readable.fromWeb(csv.stream() as any)
-    .pipe(fastcsv.parse({ headers: true }))
-    .on('error', error => console.error(error))
-    .on('data', row => processRow(row))
-    .on('end', (rowCount: number) => console.log(`Parsed ${rowCount} rows`));
+  const { csv } = formValidationResult.data;
+  const holders:Holder[] = [];
+  const rowErrors:any[] = [];
 
   try {
+    const result = await new Promise<any>((resolve, reject) => {
+      Readable.fromWeb(csv.stream() as any)
+        .pipe(parse({ headers: true }))
+        .validate((row, cb): void => {
+          const result = CsvSchema.safeParse(row);
+          if (!result.success) {
+            const reason = JSON.stringify(result.error.flatten().fieldErrors)
+            return cb(null, false, reason);
+          } 
+          return cb(null, true);
+        })
+        .on("data-invalid", (row, rowNumber, reason) => {
+            rowErrors.push(`Row #${rowNumber} ${JSON.stringify(row)} - PROBLEM: ${reason}`)      
+        })
+        .on('data', row => holders.push(row))
+        .on('error', error => reject(error))
+        .on('end', (rowCount: number) => resolve({rowCount}))
+  });
+} catch (e) {
+  console.log("an error with the CSV file parsing")
+  return {
+        errors: {
+          errorType: 'csv-parsing',
+          csvError: e,
+          message: `Failed to process your CSV file because of error: ${e} `
+        },
+        formData: incomingFormValues
+      };
+}
 
-    const result = await callStore('credential', 'POST', )
+   // check if there were zod errors
+   if (rowErrors.length) {
+      return {
+        errors: {
+          errorType: 'csv-row',
+          rowErrors,
+          message: 'Failed to process your CSV file. There were errors with the following rows: '
+        },
+        formData: incomingFormValues
+      };
+    }
 
+    // check for email duplicates in the uploaded csv rows
+    const emailAddresses : string[] = holders.map(holder=>holder.email);
+    const duplicates = emailAddresses.filter((email, index) => {
+    			// If the first index of the item is not the current index, it's a duplicate
+    			return emailAddresses.indexOf(email) !== index;
+		});
+
+    // if duplicates return an error and the list of duplicates
+    if (duplicates.length) {
+      return {
+        errors: {
+          errorType: 'duplicate-emails',
+          duplicates,
+          message: "Couldn't process your CSV file. There are rows with the same email address. Please remove the duplicates and try again. Duplicates: "
+        },
+        formData: incomingFormValues
+    };
+    }
+
+
+   //  check the database for duplicate email addresses
+  try {
+
+    const result = await callStore('holders/duplicates', 'POST', emailAddresses)
+    if (result.length) {
+      const existingHolders = result.map((holder:any)=>`${holder.email} - ${holder.name}`)
+      return {
+        errors: {
+          errorType: 'existing-holders',
+          existingHolders,
+          message: "Holders arleady exist in the database for some of the email addresses you've provided. Please correct and try again. Duplicates: "
+        },
+        formData: incomingFormValues
+    };
+    }
   } catch (error) {
-    // We'll also log the error to the console for now
     console.error(error);
     return {
-      message: 'Database Error: Failed to upload credentials.',
+      errors: {
+        errorType: 'network',
+        message: 'Database Error: Problem calling the store.'
+      }
     };
   }
+
+  // Finally, upload to the store
+
 
   revalidatePath('/dashboard/holders');
   redirect('/dashboard/holders');
