71c28dffdfd4666a488464911465b276cc9b7f2c
James Chartrand <jc.chartrand@gmail.com>
2026-03-23 16:11:44 -0400

    add status field and fix repopulation on error

--- app/ui/credentials/create-form.tsx
+++ app/ui/credentials/create-form.tsx
@@ -6,7 +6,9 @@ import {
   UserCircleIcon,
   AtSymbolIcon,
   TagIcon,
-  MagnifyingGlassCircleIcon
+  MagnifyingGlassCircleIcon,
+  CheckIcon,
+  EyeSlashIcon
 } from '@heroicons/react/24/outline';
 import { Button } from '@/app/ui/button';
 import { createCredential, State } from '@/app/lib/credentials/actions';
@@ -15,7 +17,7 @@ import ClientPortal from '../utils/clientPortal';
 import HolderLookupModal from '../holders/HolderLookupModal';
 
 export default function Form({ templates, tenants, tags }: { templates: Template[], tenants: Tenant[], tags: Tag[] }) {
-  const initialState: State = { message: null, errors: {}, formData: {credName: undefined, tenantId: undefined, templateId: undefined, tagId: undefined}   };
+  const initialState: State = { message: null, errors: {}, formData: {credName: undefined, tenantId: 'select', holderId: undefined, status: undefined, templateId: 'select', tagId: 'select'}   };
   const [state, formAction] = useActionState(createCredential, initialState);
   const [isModalOpen, setIsModalOpen] = useState(false)
   const [holder, setHolder] = useState({name:'', did: '', org_id: '', email: '', id: undefined})
@@ -61,9 +63,10 @@ export default function Form({ templates, tenants, tags }: { templates: Template
               name="templateId"
               className="peer block w-full cursor-pointer rounded-md border border-gray-200 py-2 pl-10 text-sm outline-2 placeholder:text-gray-500"
               defaultValue={state.formData?.templateId}
+              key={state.formData?.templateId}
               aria-describedby="template-id-error"
             >
-              <option value="" disabled>
+              <option value="select" >
                 Select a template
               </option>
               {templates.map((template) => (
@@ -95,9 +98,10 @@ export default function Form({ templates, tenants, tags }: { templates: Template
               name="tenantId"
               className="peer block w-full cursor-pointer rounded-md border border-gray-200 py-2 pl-10 text-sm outline-2 placeholder:text-gray-500"
               defaultValue={state.formData?.tenantId}
+              key={state.formData?.tenantId}
               aria-describedby="tenant-id-error"
             >
-              <option value="" disabled>
+              <option value="select">
                 Select an issuer
               </option>
               {tenants.map((tenant) => (
@@ -132,9 +136,10 @@ export default function Form({ templates, tenants, tags }: { templates: Template
               name="tagId"
               className="peer block w-full cursor-pointer rounded-md border border-gray-200 py-2 pl-10 text-sm outline-2 placeholder:text-gray-500"
               defaultValue={state.formData?.tagId}
+              key={state.formData?.tagId}
               aria-describedby="tag-id-error"
             >
-              <option value="" disabled>
+              <option value="select">
                 Select a tag
               </option>
               {tags.map((tag) => (
@@ -154,6 +159,7 @@ export default function Form({ templates, tenants, tags }: { templates: Template
               ))}
           </div>
         </div>
+
 
 
  {/* Credential Name */}
@@ -189,7 +195,7 @@ export default function Form({ templates, tenants, tags }: { templates: Template
         {/* Holder Name */}
         <div className="mb-4">
           <label htmlFor="holder" className="mb-2 block text-sm font-medium">
-            Choose a holder (click to search)
+            Choose a holder (click the field to search)
           </label>
           <div className="relative mt-2 rounded-md">
             <div className="relative">
@@ -261,6 +267,58 @@ export default function Form({ templates, tenants, tags }: { templates: Template
             </div>
           </div>
         </div>
+
+
+{/*  Status */}
+        <fieldset>
+          <legend className="mb-2 block text-sm font-medium">
+            Set the status (whether the holder can see and collect the credential)
+          </legend>
+          <div className="rounded-md border border-gray-200 bg-white px-[14px] py-3">
+            <div className="flex gap-4">
+              <div className="flex items-center">
+                <input
+                  id="hidden"
+                  name="status"
+                  type="radio"
+                  value="hidden"
+                  defaultChecked={state.formData?.status === 'hidden'}
+                  className="text-white-600 h-4 w-4 cursor-pointer border-gray-300 bg-gray-100 focus:ring-2 peer"
+                />
+                <label
+                  htmlFor="hidden"
+                  className="ml-2 flex cursor-pointer items-center gap-1.5 rounded-full bg-gray-500 peer-checked:bg-red-500 px-3 py-1.5 text-xs font-medium text-white"
+                >
+                  Hidden <EyeSlashIcon className="h-4 w-4" />
+                </label>
+              </div>
+              <div className="flex items-center">
+                <input
+                  id="collectable"
+                  name="status"
+                  type="radio"
+                  value="collectable"
+                  defaultChecked={state.formData?.status === 'collectable'}
+                  className="h-4 w-4 cursor-pointer border-gray-300 bg-gray-100 text-gray-600 focus:ring-2 peer"
+                />
+                <label
+                  htmlFor="collectable"
+                  className="ml-2 flex cursor-pointer items-center gap-1.5 rounded-full bg-gray-500 peer-checked:bg-green-500 px-3 py-1.5 text-xs font-medium text-white"
+                >
+                  Collectable <CheckIcon className="h-4 w-4" />
+                </label>
+              </div>
+            </div>
+          </div>
+          <div id="status-error" aria-live="polite" aria-atomic="true">
+            {state.errors?.status &&
+              state.errors.status.map((error: string) => (
+                <p className="mt-2 text-sm text-red-500" key={error}>
+                  {error}
+                </p>
+              ))}
+          </div>
+        </fieldset>
 
 
           <div id="general-error" aria-live="polite" aria-atomic="true">
