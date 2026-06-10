4339b4ca0f2d7aaf5e42456949806adc904aa6e3
James Chartrand <jc.chartrand@gmail.com>
2026-06-10 16:14:19 -0400

    reorganize main page

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -12,41 +12,38 @@ import Image from "next/image";
 
 const categories = [
   {
-    name: "Manage Credentials",
+    name: "Add and Manage Credentials",
     href: "/dashboard/credentials",
     icon: DocumentDuplicateIcon,
   },
+  { name: "Add and Manage Credential Holders (Recipients)", href: "/dashboard/holders", icon: UserGroupIcon },
+  { name: "Add and Manage Tags (Categories)", href: "/dashboard/tags", icon: TagIcon },
   { name: "View Reports", href: "/dashboard/reports", icon: ChartBarIcon },
-  { name: "Manage Tags", href: "/dashboard/tags", icon: TagIcon },
-  { name: "Manage Tenants", href: "/dashboard/tenants", icon: BuildingOfficeIcon },
-  { name: "Manage Users", href: "/dashboard/users", icon: UserGroupIcon },
+  { name: "Add and Manage Tenants (Issuers)", href: "/dashboard/tenants", icon: BuildingOfficeIcon },
+  { name: "Add and Manage Dashboard Users", href: "/dashboard/users", icon: UserGroupIcon },
 ];
 
 export default async function Page() {
   return (
     <main className="flex min-h-screen flex-col p-6">
-      <div className="mt-4 flex flex-row gap-4 rounded-lg bg-gray-50">
-       
+      <div className="mt-4 flex flex-row gap-4 items-center justify-center rounded-lg bg-gray-50">
+       <div className="flex flex-row items-center justify-center gap-4 p-6 ">
           <p
-            className={`${lusitana.className} text-l text-center text-gray-800 `}
+            className={`${lusitana.className} text-2xl text-center text-gray-800 `}
           >
-            <strong>Welcome to your Issuer Dashboard.</strong> 
-            <div>Brought to you by
-            the {" "}
-            <a href="https://dcconsortium.org" className="text-blue-500">
-              Digital Credentials Commons
-            </a>
-            </div>
+          Issuer Dashboard
+       
           </p>
+          </div>
             <Image
               src="/landing-page-desktop.png"
-              width={500}
-              height={380}
+              width={250}
+              height={190}
               alt="Screenshots of the dashboard project showing desktop version"
             />
       
       </div>
-      <div className="mt-6 grid grid-cols-2 gap-4 sm:grid-cols-3 lg:grid-cols-5">
+      <div className="mt-6 grid grid-cols-3 gap-4 sm:grid-cols-3 ">
         {categories.map(({ name, href, icon: Icon }) => (
           <Link
             key={name}
